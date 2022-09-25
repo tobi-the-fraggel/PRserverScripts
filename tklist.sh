1c64198c54b343c0be05c48e75538dcd46e9e352
tobi <tsiebke@gmx.de>
2022-09-25 08:14:22 +0200

    indent. format changed to 4 spaces

--- tklist.sh
+++ tklist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-###
+#
 #Purpose: Find and count TK'ers from a given time
 #
 #CAUCTION: Script not made portable must be executed from within /logs/
@@ -24,10 +24,10 @@ TDD=$(date +"%Y%m%d")
 
 #grabbing TK'ers names file by file and save them in a log.
 for WFL in "chatlog_"$TDD"_"*; do
-        echo "+++++Tk's on $WFL+++++" >> $TKLIST
-        grep TEAMKILLS $WFL | cut -d' ' -f4 | sort | uniq -c | sort -n | sed 's/^[ \t]*//g' >> $TKLIST
-        echo "+++++End++++++" >> $TKLIST
-        echo "" >> $TKLIST
+    echo "+++++Tk's on $WFL+++++" >> $TKLIST
+    grep TEAMKILLS $WFL | cut -d' ' -f4 | sort | uniq -c | sort -n | sed 's/^[ \t]*//g' >> $TKLIST
+    echo "+++++End++++++" >> $TKLIST
+    echo "" >> $TKLIST
 done
 
 cat "$TKLIST" | mail -s $TDD $adress
